91d677cd96a86b2715b81a33bbef7299959f7103
Sebastian Gonzales <sebastian.gt.2002@gmai.com>
2024-04-13 12:29:06 -0300

    [Parte 4.3] Agrego la plantilla de results

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -11,13 +11,13 @@ def index(request):
     context = {"latest_question_list": latest_question_list}
     return render(request, "polls/index.html", context)
 
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
+
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, "polls/detail.html", {"question": question})
-
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
 
 
 def vote(request, question_id):
