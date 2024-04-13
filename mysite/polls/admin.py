7edb554e517a507d4dac62eb21c8e18d48a1920d
Sebastian Gonzales <sebastian.gt.2002@gmai.com>
2024-04-13 02:36:45 -0300

    [Parte 2.5] Registro el modelo Question en la página de administación

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import Question
+
 # Register your models here.
+
+admin.site.register(Question) # Para hacer que nuestro modelo sea visible en la página de administración, tenemos que registrar el modelo con admin.site.register(Question)
