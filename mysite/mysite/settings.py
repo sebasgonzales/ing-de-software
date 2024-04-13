b490a9d13f08aae9de31807cbdd20569d41ed424
Sebastian Gonzales <sebastian.gt.2002@gmai.com>
2024-04-13 01:32:23 -0300

    [Parte 2.2] Agrego configuración de la aplicación de encuestas

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "polls.apps.PollsConfig", # PollsConfig se define en el archivo polls/apps.py
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
